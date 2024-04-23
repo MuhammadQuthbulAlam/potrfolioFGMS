fcf93016cacc0b1c9937d849091019c1bb4a0c1b
Muhammad Quthbul Alam <100410118+MuhammadQuthbulAlam@users.noreply.github.com>
2024-04-23 18:44:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
       if (this.scrollY > 20) {
         $(".navbar").addClass("sticky");
       } else {
-        $(".navbar").removeClass("sticky");
+        $(".navbar").removeClass("sticky");  
       }
   
       //  scroll-up button show/hide script  //
